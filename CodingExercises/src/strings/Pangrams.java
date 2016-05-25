512244d6e9f7f04b3dc4ef5156cb579ca6de8b27
anveragi <anveragi@192.168.1.62>
2016-05-24 18:41:31 -0700

    Completed strings.Pangrams

--- CodingExercises/src/strings/Pangrams.java
+++ CodingExercises/src/strings/Pangrams.java
@@ -3,7 +3,37 @@ package strings;
 import java.util.Scanner;
 
 public class Pangrams {
-
+	
+	boolean isPangram(String str) {
+		
+		String string = str.toLowerCase().replaceAll("\\s", "");
+		
+		int chararray[] = new int[26];
+		
+//		System.out.println(string);
+		
+		for(int i=0;i<string.length();i++) {
+			int c = string.charAt(i);
+//			System.out.println(c);
+			if(c-97 >= 0 && c-97 <= 122) {
+				chararray[c-97]++;
+			}
+			else {
+				System.out.println("Please enter only letters, not numbers of special characters in the string!!");
+				System.exit(0);
+			}
+		}
+		
+		for(int i=0;i<26;i++) {
+			if(chararray[i] == 0)
+				return false;
+			else
+					continue;
+		}
+		
+		return true; 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -15,8 +45,17 @@ public class Pangrams {
 		
 		sc.close();
 		
-		System.out.println(str);
+		if(str.length() > 1000)
+			System.out.println("String length exceeded limit!! Enter less number of characters (< 1000)");
 		
+		Pangrams pangrams = new Pangrams();
+		
+		boolean result = pangrams.isPangram(str);
+		
+		if(result)
+			System.out.println("'" + str + "' is a Pangram!");
+		else
+			System.out.println("'" + str + "' is not a Pangram!");
 	}
 
 }
